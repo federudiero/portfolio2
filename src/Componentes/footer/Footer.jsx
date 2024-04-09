81f2974c58dd3a15e7e673550bc815abd44bccff
federudiero <federudiero@gmail.com>
2024-04-09 09:18:52 -0300

    cambio footer

--- src/Componentes/footer/Footer.jsx
+++ src/Componentes/footer/Footer.jsx
@@ -4,15 +4,10 @@ import CodeIcon from '@mui/icons-material/Code';
 import { Link } from 'react-router-dom';
 
 function Footer() {
+  
   return (
     <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
       <nav className="grid grid-flow-col gap-4">
-        <Link
-          to={`/nosotros`}
-          className="btn btn-ghost text-xl"
-        >
-          Nosotros
-        </Link>
         <Link
           to={`/contacto`}
           className="btn btn-ghost text-xl"
